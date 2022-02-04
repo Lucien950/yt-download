ccb146e5264878049f92489d988d7a5250e10de1
Edwin <jessicawu414@gmail.com>
2022-02-04 17:07:38 -0500

    GCP Deployment

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 let apiUrl: string;
 // TODO Deploy machine and get url
 if (process.env.NODE_ENV == "production") {
-    // apiUrl = "https://[URL].com/api"; 
+    apiUrl = "https://api.yt-download.com"; 
 } else {
     apiUrl = "http://localhost:3001";
 }
