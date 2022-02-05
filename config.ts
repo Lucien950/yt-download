e8f8e00e70a13bc60c014282bc60e940d4fdd106
Edwin <jessicawu414@gmail.com>
2022-02-05 00:42:07 -0500

    Port now 8080

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 let apiUrl: string;
 // TODO Deploy machine and get url
-if (process.env.NODE_ENV == "development") apiUrl = "http://localhost:3001"
+if (process.env.NODE_ENV == "development") apiUrl = "http://localhost:8080"
 else apiUrl = "https://api.yt-download.com"
 
 const ytPublic = "AIzaSyBdipjRgFltnuUknzQLgQXYNpW5y7TPqXE"
