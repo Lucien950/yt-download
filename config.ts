e345981e90920770daf38e11697fdef0fc4a02b4
Edwin <jessicawu414@gmail.com>
2022-02-05 12:15:40 -0500

    Implemented Ads

--- config.ts
+++ config.ts
@@ -1,8 +1,3 @@
-let apiUrl: string;
-// TODO Deploy machine and get url
-if (process.env.NODE_ENV == "development") apiUrl = "http://localhost:8000"
-else apiUrl = "https://api.yt-download.ca"
-
+const apiUrl = "https://api.yt-download.ca"
 const ytPublic = "AIzaSyBdipjRgFltnuUknzQLgQXYNpW5y7TPqXE"
-
 export {apiUrl, ytPublic};
