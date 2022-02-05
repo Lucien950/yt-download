99336266d69421b3e18c967c6f6e028f79345763
Edwin <jessicawu414@gmail.com>
2022-02-05 00:27:21 -0500

    Emergency YouTube API Patch

--- config.ts
+++ config.ts
@@ -1,11 +1,8 @@
 let apiUrl: string;
 // TODO Deploy machine and get url
-if (process.env.NODE_ENV == "production") {
-    apiUrl = "https://api.yt-download.com"; 
-} else {
-    apiUrl = "http://localhost:3001";
-}
+if (process.env.NODE_ENV == "development") apiUrl = "http://localhost:3001"
+else apiUrl = "https://api.yt-download.com"
 
-const ytPublic = "AIzaSyDKQZqGIoVN33wujCfnr0ETXXME0DUtXwI"
+const ytPublic = "AIzaSyBdipjRgFltnuUknzQLgQXYNpW5y7TPqXE"
 
 export {apiUrl, ytPublic};
