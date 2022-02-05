81142b10ca2f8d16444229edbf33429cd76eb5e0
Edwin <jessicawu414@gmail.com>
2022-02-05 00:43:16 -0500

    API WAS POINTING TO COM😭😭

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 let apiUrl: string;
 // TODO Deploy machine and get url
 if (process.env.NODE_ENV == "development") apiUrl = "http://localhost:8080"
-else apiUrl = "https://api.yt-download.com"
+else apiUrl = "https://api.yt-download.ca"
 
 const ytPublic = "AIzaSyBdipjRgFltnuUknzQLgQXYNpW5y7TPqXE"
 
